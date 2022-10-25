3545f539b8d6d0d64e2d40349efe2e8f659b2f10
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-10-25 14:13:05 +0600

    blog completed

--- src/components/Darkmode.js
+++ src/components/Darkmode.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import DarkModeToggle from 'react-dark-mode-toggle'
 
 const Darkmode = () => {
-  const [isDarkMode, setIsDarkMode] = useState(() => false)
+  const [isDarkMode, setIsDarkMode] = useState(() => true)
   return (
     <div className="flex items-center">
       <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={50} />
