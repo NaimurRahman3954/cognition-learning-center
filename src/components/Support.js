2aa5dedfef677e17d0488559fc5b45e8d47e0f14
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-16 02:09:11 +0600

    course page updated

--- src/components/Support.js
+++ src/components/Support.js
@@ -67,6 +67,246 @@ const Support = () => {
           <div className="stat-desc">99% students recommend to their peers</div>
         </div>
       </div>
+      {/* -------- */}
+      {/* <!-- component --> */}
+      <div class="flex antialiased">
+        <div class="w-full overflow-x-hidden">
+          <div class="flex flex-col flex-auto h-full p-6 text-white">
+            <div class="flex flex-col flex-auto flex-shrink-0 rounded-2xl outline outline-1 outline-slate-700 h-full p-4">
+              <div class="flex flex-col h-full overflow-x-auto mb-4">
+                <div class="flex flex-col h-full">
+                  <div class="grid grid-cols-12 gap-y-2">
+                    <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                      <div class="flex flex-row items-center">
+                        <div class="flex items-center justify-center h-10 w-10 rounded-full  bg-red-400 flex-shrink-0">
+                          M
+                        </div>
+                        <div class="relative ml-3 text-sm bg-base-300 py-2 px-4 shadow rounded-xl">
+                          <div>
+                            Hi, I'm trying to enroll in a course but I'm having
+                            trouble adding it to my cart.
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                      <div class="flex items-center justify-start flex-row-reverse">
+                        <div class="flex items-center justify-center h-10 w-10 rounded-full bg-sky-500 flex-shrink-0">
+                          N
+                        </div>
+                        <div class="relative mr-3 text-sm bg-blue-600 py-2 px-4 shadow rounded-xl">
+                          <div>
+                            Hi, I'm sorry to hear that you're having trouble
+                            enrolling in a course. Can you tell me which course
+                            you're trying to enroll in and what steps you're
+                            taking to add it to your cart?
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                      <div class="flex flex-row items-center">
+                        <div class="flex items-center justify-center h-10 w-10 rounded-full  bg-red-400 flex-shrink-0">
+                          M
+                        </div>
+                        <div class="relative ml-3 text-sm bg-base-300 py-2 px-4 shadow rounded-xl">
+                          <div>
+                            I'm trying to enroll in the "Data Structures and
+                            Algorithm" course. I click on the "enroll" button,
+                            but when I try to add it to my cart, nothing
+                            happens.
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                      <div class="flex flex-row items-center">
+                        <div class="flex items-center justify-center h-10 w-10 rounded-full  bg-red-400 flex-shrink-0">
+                          M
+                        </div>
+                        <div class="relative ml-3 text-sm bg-base-300 py-2 px-4 shadow rounded-xl">
+                          <div class="flex flex-row items-center">
+                            <button class="flex items-center justify-center bg-indigo-600 hover:bg-indigo-800 rounded-full h-8 w-10">
+                              <svg
+                                class="w-6 h-6 text-white"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  stroke-width="1.5"
+                                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                                ></path>
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  stroke-width="1.5"
+                                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                ></path>
+                              </svg>
+                            </button>
+                            <div class="flex flex-row items-center space-x-px ml-4">
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-4 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-12 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-6 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-5 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-4 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-3 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-1 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-1 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
+                              <div class="h-4 w-1 bg-gray-500 rounded-lg"></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                      <div class="flex items-center justify-start flex-row-reverse">
+                        <div class="flex items-center justify-center h-10 w-10 rounded-full  bg-sky-500 flex-shrink-0">
+                          N
+                        </div>
+                        <div class="relative mr-3 text-sm bg-blue-600 py-2 px-4 shadow rounded-xl">
+                          <div>
+                            I apologize for the inconvenience. Can you please
+                            try clearing your browser cache and cookies and
+                            trying again? This can sometimes resolve issues like
+                            this.
+                          </div>
+                          <div class="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500">
+                            Seen
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                      <div class="flex flex-row items-center">
+                        <div class="flex items-center justify-center h-10 w-10 rounded-full  bg-red-400 flex-shrink-0">
+                          M
+                        </div>
+                        <div class="relative ml-3 text-sm bg-base-300 py-2 px-4 shadow rounded-xl">
+                          <div>
+                            That worked, thank you! I was able to add the course
+                            to my cart and complete the enrollment process.
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                      <div class="flex items-center justify-start flex-row-reverse">
+                        <div class="flex items-center justify-center h-10 w-10 rounded-full  bg-sky-500 flex-shrink-0">
+                          N
+                        </div>
+                        <div class="relative mr-3 text-sm bg-blue-600 py-2 px-4 shadow rounded-xl">
+                          <div>
+                            You're welcome! I'm glad I could help. If you have
+                            any other questions or issues, don't hesitate to
+                            reach out to us.
+                          </div>
+                          <div class="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500">
+                            Seen
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-row items-center h-16 rounded-xl bg-base-300 w-full px-4">
+                <div>
+                  <button class="flex items-center justify-center text-gray-400 hover:text-gray-600">
+                    <svg
+                      class="w-5 h-5"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+                <div class="flex-grow ml-4">
+                  <div class="relative w-full">
+                    <input
+                      type="text"
+                      class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                    />
+                    <button class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+                      <svg
+                        class="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div class="ml-4">
+                  <button class="flex items-center justify-center  bg-red-400 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+                    <span>Send</span>
+                    <span class="ml-2">
+                      <svg
+                        class="w-4 h-4 transform rotate-45 -mt-px"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                        ></path>
+                      </svg>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
