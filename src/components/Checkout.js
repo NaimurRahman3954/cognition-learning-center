2a63bf32a1b85acb97df05ea4e521320def02188
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-17 23:54:03 +0600

    course page updated

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -8,13 +8,13 @@ const Checkout = () => {
     <div className="hero min-h-screen">
       <div className="card bg-base-300 text-neutral-content mb-12 p-9 mx-3 outline outline-1 outline-slate-700">
         <div className="card-body items-center text-center">
-          <h2 className="card-title text-4xl font-bold">{title}</h2>
+          <h2 className="card-title text-2xl lg:text-4xl font-bold">{title}</h2>
           <div className="badge badge-info font-bold my-6 p-3 mb-12">
             Price: ৳ {price} only
           </div>
           <div className="card-actions justify-end">
             <button className="btn btn-ghost">Cancel</button>
-            <button className="btn btn-primary">Proceed to payment</button>
+            <button className="btn btn-warning">Proceed to payment</button>
           </div>
         </div>
       </div>
