cee8f6020424412c4c61e222f02fb998d3472219
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-17 23:55:42 +0600

    course page updated

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 const FAQ = () => {
   return (
     <div>
-      <div className="hero min-h-screen bg-base-100 bg-dotted-spacing-4 bg-dotted-gray-700">
+      <div className="hero min-h-screen bg-base-100 bg-dotted-spacing-4 bg-dotted-gray-800">
         <div className="hero-content text-center mb-12">
           <div className="max-w-md">
             <h1 className="text-3xl font-bold">Hello there!</h1>
             <p className="py-6">FAQ page will be available soon.</p>
             <Link to="/courses">
-              <button className="btn btn-primary mb-12">Explore Courses</button>
+              <button className="btn btn-warning mb-12">Explore Courses</button>
             </Link>
           </div>
         </div>
