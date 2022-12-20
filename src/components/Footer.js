2f458051f117057ea2ebed142a998c5232402abc
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-20 23:32:57 +0600

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,20 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded outline outline-t-1 outline-slate-800">
+      <footer className="footer footer-center py-24 p-10 bg-base-200 text-base-content rounded outline outline-t-1 outline-slate-800">
         <div className="grid grid-flow-col gap-4">
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a href="/" className="link link-hover">
+            Contact
+          </a>
+          <a href="/" className="link link-hover">
+            About us
+          </a>
+          <a href="/" className="link link-hover">
+            Jobs
+          </a>
+          <a href="/" className="link link-hover">
+            Press kit
+          </a>
         </div>
         <div>
           <div className="grid grid-flow-col gap-4">
