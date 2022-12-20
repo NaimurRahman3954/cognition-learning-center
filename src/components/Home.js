89b4e4826c9acd52a17b2ac84970ce5907fbb38d
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-20 21:21:33 +0600

    hero section responsiveness fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,11 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import Hero from './Hero'
 
 const Home = () => {
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-100 bg-dotted-spacing-4 bg-dotted-gray-700">
-        <div className="hero-content flex-col lg:flex-row-reverse mb-12">
-          <img
-            src="https://content.techgig.com/photo/77087595/Guide-How-to-build-career-as-a-programmer-without-college-degree.jpg"
-            className="max-w-lg rounded-lg shadow-2xl"
-            alt="#"
-          />
-          <div className="m-5">
-            <h1 className="text-5xl font-bold">Cognition Learning Center!</h1>
-            <p className="py-6">
-              The best programming boot-camp in Bangladesh 🇧🇩 <br />
-            </p>
-            <Link to="/courses">
-              <button className="btn btn-primary">Explore Courses</button>
-            </Link>
-          </div>
-        </div>
-      </div>
+    <div className="container mx-auto">
+      <Hero></Hero>
     </div>
   )
 }
