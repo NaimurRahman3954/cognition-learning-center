b4acdf06f8d549d21b4da8d2e9bac90250cea1b8
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-20 21:32:46 +0600

    course page  responsiveness fixed

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -14,11 +14,11 @@ const Courses = () => {
 
   return (
     <div className="flex mb-12">
-      <div className="basis-1/3 lg:m-5 lg:p-5">
+      <div className="basis-1/3 lg:m-5 lg:p-5 lg:pr-0 lg:mr-0 hidden lg:block">
         <LeftSideNav></LeftSideNav>
       </div>
-      <div className="basis-2/3">
-        <div className="flex flex-wrap align-middle">
+      <div className="lg:basis-2/3 ">
+        <div className="flex flex-wrap align-middle justify-center">
           {courses.map((course) => (
             <Card key={course.id} course={course}></Card>
           ))}
